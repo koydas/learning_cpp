b684b7adac2ad606e6bda1a32802cc4eec9737fe
Stephane Hamel <SHamel@maestro.ca>
2019-05-02 09:38:36 -0400

    remove cached files

--- Console-Menu/Main.h
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <string>
-
-#pragma once
-
-void displayMenu();
-void displayResult(bool valid, std::string choiceString);
-int getNumericValue(std::string choice);
-bool setChoiceString(std::string choice, std::string &choiceString);
