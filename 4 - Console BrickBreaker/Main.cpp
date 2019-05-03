2b2056dfad0ffaee4a0c02c7deb7551fdf33896f
Stéphane Hamel <koydas@hotmail.com>
2019-05-02 21:00:12 -0400

    ball mouvement

--- 4 - Console BrickBreaker/Main.cpp
+++ 4 - Console BrickBreaker/Main.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 #include <iostream>
 #include <string>
+#include <windows.h>
 
 const int width = 14;
 const int height = 7;
@@ -10,13 +11,13 @@ const int height = 7;
 enum tileType { wall, brick, ball, paddle, none };
 
 tileType tilemap[height][width] = {
-{wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall },
-{wall, brick,brick,brick,brick,brick,brick,brick,brick,brick,brick,brick,brick, wall},
-{wall, brick,brick,brick,brick,brick,brick,brick,brick,brick,brick,brick,brick, wall},
-{wall, none, none, none, none, none, none, none, none, none, none, none, none, wall},
-{wall, none, none, none, none, none, none, none, none, none, none, none, none, wall},
-{wall, none, none, none, none, none, none, none, none, none, none, none, none, wall},
-{wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall },
+	{wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall, wall },
+	{wall, brick,brick,brick,brick,brick,brick,brick,brick,brick,brick,brick,brick, wall},
+	{wall, brick,brick,brick,brick,brick,brick,brick,brick,brick,brick,brick,brick, wall},
+	{wall, none, none, none, none, none, none, none, none, none, none, none, none, wall},
+	{wall, none, none, none, none, none, none, none, none, none, none, none, none, wall},
+	{wall, none, none, none, none, none, none, none, none, none, none, none, none, wall},
+	{wall, none, none, none, none, none, none, none, none, none, none, none, none, wall}
 };
 
 bool quit = false;
@@ -25,10 +26,11 @@ const int paddleStartX = 6;
 int paddleX = paddleStartX;
 int paddleWidth = 3;
 
-const int ballStartX = 7;
-const int ballStartY = 7;
-
-int ballX = ballStartX;
+bool ballFreezed = true;
+int ballX;
+int ballY;
+float ballVelocityX = 0;
+float ballVelocityY = 0;
 
 int score = 0;
 
@@ -44,7 +46,11 @@ int main() {
 void update() {
 	input();
 
+	clearMovingElements();
 	updatePaddlePosition();
+	updateBallPosition();
+
+	Sleep(500);
 }
 
 void draw() {
@@ -65,6 +71,9 @@ void draw() {
 					break;
 				case paddle:
 					s = "-";
+					break;
+				case ball:
+					s = "O";
 					break;
 				case none:
 					s = " ";
@@ -94,20 +103,51 @@ void input() {
 			if (paddleX+4 < width)
 				paddleX++;
 			break;
+		case ' ':
+			if (ballFreezed) {
+				ballVelocityX = -1;
+				ballVelocityY = -1;
+
+				ballFreezed = false;
+			}
+			break;
+		}
+	}
+}
+
+void clearMovingElements() {
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++) {
+			if (tilemap[i][j] == paddle || tilemap[i][j] == ball) {
+				tilemap[i][j] = none;
+			}
 		}
 	}
 }
 
 void updatePaddlePosition() {
-	for (int i = 0; i < height; i++) {
-		for (int j = 0; j < width; j++) {
-			if (tilemap[i][j] == paddle) {
-				tilemap[i][j] = none;
-			}
-		}
+	tilemap[6][paddleX] = paddle;
+	tilemap[6][paddleX + 1] = paddle;
+	tilemap[6][paddleX + 2] = paddle;
+}
+
+void updateBallPosition() {
+	if (ballFreezed) {
+		ballX = paddleX + 1;
+		ballY = 5;
+	}
+	else {
+		ballX += ballVelocityX;
+		ballY += ballVelocityY;
 	}
 
-	tilemap[5][paddleX] = paddle;
-	tilemap[5][paddleX + 1] = paddle;
-	tilemap[5][paddleX + 2] = paddle;
+	if (tilemap[ballY][ballX] == none) {
+		tilemap[ballY][ballX] = ball;
+	}
+	else {
+		ballVelocityX *= -1;
+		ballVelocityY *= -1;
+	}
+
+	
 }
--- 4 - Console BrickBreaker/main.h
+++ 4 - Console BrickBreaker/main.h
@@ -1,4 +1,7 @@
 void update(); 
 void draw();
 void input();
+
+void clearMovingElements();
 void updatePaddlePosition();
+void updateBallPosition();
